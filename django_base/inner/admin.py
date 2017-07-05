fa16cc83d1bffe5d8d784dc56e124207ed67456c
Windson <wiwindson@outlook.com>
2017-07-05 10:05:07 +0800

    add admin order

--- django_base/inner/admin.py
+++ django_base/inner/admin.py
@@ -7,4 +7,7 @@ class InnerAdmin(admin.ModelAdmin):
     list_display = ('name', 'outer', 'id', 'create_time',)
     search_fields = ['name', 'outer__name']
 
+    def get_ordering(self, request):
+        return ['outer']
+
 admin.site.register(Inner, InnerAdmin)
