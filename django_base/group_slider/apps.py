5308f4052b7147c86054a5fd0d208bd84eeae3b5
Windsooon <wiwindson@outlook.com>
2017-07-07 07:50:44 +0800

    add group slider

---
+++ django_base/group_slider/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GroupSliderConfig(AppConfig):
+    name = 'group_slider'
---
+++ django_base/group_slider/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from inner.models import Inner
+
+
+class GroupSlider(models.Model):
+    inner = models.ForeignKey(
+        Inner, related_name="group_slider", on_delete=models.CASCADE)
+    title = models.CharField(max_length=64, blak=True)
+    content = models.CharField(max_length=256, blank=True)
+    thumbnail = models.CharField(max_length=256, blank=True)
+    url = models.CharField(max_length=256, blank=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
