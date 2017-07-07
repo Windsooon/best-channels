9ee97dc25fdb507594b0cb39dcd12098af6ac84b
Windson <wiwindson@outlook.com>
2017-07-07 11:58:47 +0800

    fix urls bug

--- django_base/django_base/urls.py
+++ django_base/django_base/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'channel/(?P<title>.+)/$', views.channel, name='channel'),
-    url(r'category/(?P<title>.+)/$', views.category, name='category'),
     url(r'subcategory/(?P<sub>.+)/$', views.sub_category, name='sub_category'),
+    url(r'category/(?P<title>.+)/$', views.category, name='category'),
     url(r'^api/', include('api.urls')),
     url(r'^api-auth/', include(
         'rest_framework.urls', namespace='rest_framework')),
