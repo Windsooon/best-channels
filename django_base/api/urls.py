84bdb4a3161a9fd3c21eaac0d8cb06c346715175
Windsooon <wiwindson@outlook.com>
2017-07-10 23:35:12 +0800

    add cache

--- django_base/api/urls.py
+++ django_base/api/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.decorators.cache import cache_page
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
@@ -56,11 +57,13 @@ urlpatterns = format_suffix_patterns([
     url(r'recommend/$', recommend_list, name='recommend_list'),
     url(r'recommend/(?P<pk>[0-9]+)/$',
         recommend_detail, name='recommend_detail'),
-    url(r'outer/$', outer_list, name='outer_list'),
+    url(r'outer/$', cache_page(60 * 200)(outer_list), name='outer_list'),
     url(r'outer/(?P<pk>[0-9]+)/$', outer_detail, name='outer_detail'),
-    url(r'inner/$', inner_list, name='inner_list'),
+    url(r'inner/$', cache_page(60 * 200)(inner_list), name='inner_list'),
     url(r'inner/(?P<pk>[0-9]+)/$', inner_detail, name='inner_detail'),
-    url(r'playlist/$', playlist_list, name='playlist_list'),
-    url(r'playlist/(?P<pk>[0-9]+)/$', playlist_detail, name='playlist_detail'),
+    url(r'playlist/$',
+        cache_page(60 * 200)(playlist_list), name='playlist_list'),
+    url(r'playlist/(?P<pk>[0-9]+)/$',
+        cache_page(60 * 200)(playlist_detail), name='playlist_detail'),
     url(r'$', views.api_root),
 ])
--- django_base/django_base/urls.py
+++ django_base/django_base/urls.py
@@ -1,14 +1,18 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.decorators.cache import cache_page
 from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'channel/(?P<title>.+)/$', views.channel, name='channel'),
-    url(r'subcategory/(?P<sub>.+)/$', views.sub_category, name='sub_category'),
-    url(r'category/(?P<title>.+)/$', views.category, name='category'),
+    url(r'channel/(?P<title>.+)/$',
+        cache_page(60 * 200)(views.channel), name='channel'),
+    url(r'subcategory/(?P<sub>.+)/$',
+        cache_page(60 * 200)(views.sub_category), name='sub_category'),
+    url(r'category/(?P<title>.+)/$',
+        cache_page(60 * 200)(views.category), name='category'),
     url(r'^api/', include('api.urls')),
     url(r'^api-auth/', include(
         'rest_framework.urls', namespace='rest_framework')),
-    url(r'^', views.cate),
+    url(r'^', cache_page(60 * 200)(views.cate)),
 ]
