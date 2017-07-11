d0b972d3b383e79450a189900b37a0dc8a84c2ea
Windsooon <wiwindson@outlook.com>
2017-07-11 22:17:17 +0800

    add frontpage cache

--- django_base/django_base/urls.py
+++ django_base/django_base/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^why/$', views.why),
+    url(r'^why/$', cache_page(60 * 100)(views.why)),
     url(r'channel/(?P<title>.+)/$',
         cache_page(60 * 200)(views.channel), name='channel'),
     url(r'subcategory/(?P<sub>.+)/$',
