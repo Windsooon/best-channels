923902636ef733ca8c6463655c93a78ea35e19ea
Windson <wiwindson@outlook.com>
2017-07-11 12:23:54 +0800

    fix cache and search

--- django_base/static/js/base.js
+++ django_base/static/js/base.js
@@ -1,50 +1,10 @@
 $(document).ready(function() {
-    var localCache = {
-        timeout: 300000,
-        data: {},
-        remove: function (url) {
-            delete localCache.data[url];
-        },
-        exist: function (url) {
-            return !!localCache.data[url] && ((new Date().getTime() - localCache.data[url]._) < localCache.timeout);
-        },
-        get: function (url) {
-            console.log('Getting in cache for url' + url);
-            return localCache.data[url].data;
-        },
-        set: function (url, cachedData, callback) {
-            localCache.remove(url);
-            localCache.data[url] = {
-                _: new Date().getTime(),
-                data: cachedData
-            };
-            if ($.isFunction(callback)) callback(cachedData);
-        }
-    };
-    $.ajaxPrefilter(function (options, originalOptions, jqXHR) {
-        if (options.cache) {
-            var complete = originalOptions.complete || $.noop,
-                url = originalOptions.url;
-            //remove jQuery cache as we have our own localCache
-            options.cache = false;
-            options.beforeSend = function () {
-                if (localCache.exist(url)) {
-                    complete(localCache.get(url));
-                    return false;
-                }
-                return true;
-            };
-            options.complete = function (data, textStatus) {
-                localCache.set(url, data, complete);
-            };
-        }
-    });
     var select_cate =  $('#select-cate').selectize({
         valueField: 'name',
         labelField: 'name',
         searchField: 'name',
         preload: true,
-        placeholder: "Search for subcategory",
+        placeholder: "Search subcategory",
         options: [],
         closeAfterSelect: true,
         create: false,
@@ -62,7 +22,7 @@ $(document).ready(function() {
         },
         onItemAdd: function (value, item) {
             var host = "http://"+ window.location.hostname;
-            window.location = host + "/group" + "/" + value + "/";
+            window.location = host + "/subcategory" + "/" + replaceSpace(value) + "/";
         },
         load: function(query, callback) {
             $.ajax({
