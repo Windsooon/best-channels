680cab2edeb0b02d84a474712b43d2f0c0b7fb95
Windson <wiwindson@outlook.com>
2017-09-05 10:59:11 +0800

    shorten cache

--- django_base/django_base/urls.py
+++ django_base/django_base/urls.py
@@ -5,17 +5,17 @@ from . import views
 
 urlpatterns = [
     url(r'^here_is_normal_admin/', admin.site.urls),
-    url(r'^why/$', cache_page(60 * 100)(views.why)),
+    url(r'^why/$', cache_page(60 * 10)(views.why)),
     url(r'channel/(?P<title>.+)/$',
-        cache_page(60 * 200)(views.channel), name='channel'),
+        cache_page(60 * 10)(views.channel), name='channel'),
     url(r'subcategory/(?P<sub>.+)/$',
-        cache_page(60 * 200)(views.sub_category), name='sub_category'),
+        cache_page(60 * 10)(views.sub_category), name='sub_category'),
     url(r'videos/(?P<title>.+)/$',
-        cache_page(60 * 200)(views.videos), name='videos'),
+        cache_page(60 * 10)(views.videos), name='videos'),
     url(r'category/(?P<title>.+)/$',
-        cache_page(60 * 200)(views.category), name='category'),
+        cache_page(60 * 10)(views.category), name='category'),
     url(r'^api/', include('api.urls')),
     url(r'^api-auth/', include(
         'rest_framework.urls', namespace='rest_framework')),
-    url(r'^', cache_page(60 * 200)(views.cate)),
+    url(r'^', cache_page(60 * 10)(views.cate)),
 ]
