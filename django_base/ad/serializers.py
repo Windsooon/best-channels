8936bd12d54123a11c10b57db43457c5d6955aef
Windsooon <wiwindson@outlook.com>
2017-09-14 23:38:53 +0800

    fix ad serializers

--- django_base/ad/serializers.py
+++ django_base/ad/serializers.py
@@ -3,10 +3,11 @@ from rest_framework import serializers
 
 
 class AdSerializer(serializers.ModelSerializer):
+    inner_name = serializers.ReadOnlyField(source='inner.name')
 
     class Meta:
         model = Ad
         fields = (
-            'id', 'inner', 'name', 'thumbnail',
+            'id', 'name', 'inner_name', 'thumbnail',
             'url', 'position', 'create_time'
         )
