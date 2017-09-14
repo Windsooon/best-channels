d1eb6460529b9dc83d9a210d44a98dc404c19016
Windsooon <wiwindson@outlook.com>
2017-09-14 23:57:07 +0800

    delete cache

--- django_base/api/urls.py
+++ django_base/api/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url
-from django.views.decorators.cache import cache_page
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
@@ -55,19 +54,19 @@ urlpatterns = format_suffix_patterns([
     url(r'recommend/$', recommend_list, name='recommend_list'),
     url(r'recommend/(?P<pk>[0-9]+)/$',
         recommend_detail, name='recommend_detail'),
-    url(r'outer/$', cache_page(60 * 10)(outer_list), name='outer_list'),
+    url(r'outer/$', outer_list, name='outer_list'),
     url(r'outer/(?P<pk>[0-9]+)/$', outer_detail, name='outer_detail'),
-    url(r'inner/$', cache_page(60 * 10)(inner_list), name='inner_list'),
+    url(r'inner/$', inner_list, name='inner_list'),
     url(r'inner/(?P<pk>[0-9]+)/$', inner_detail, name='inner_detail'),
     url(r'playlist/$',
-        cache_page(60 * 10)(playlist_list), name='playlist_list'),
+        playlist_list, name='playlist_list'),
     url(r'playlist/(?P<pk>[0-9]+)/$',
-        cache_page(60 * 10)(playlist_detail), name='playlist_detail'),
+        playlist_detail, name='playlist_detail'),
     url(r'ad/$',
-        cache_page(60 * 10)(ad_list), name='ad_list'),
+        ad_list, name='ad_list'),
     url(r'ad/(?P<pk>[0-9]+)/$',
-        cache_page(60 * 10)(ad_detail), name='ad_detail'),
+        ad_detail, name='ad_detail'),
     url(r'weekly/$',
-        cache_page(60 * 10)(weekly_list), name='weekly_list'),
-    url(r'$', cache_page(60 * 10)(views.api_root)),
+        weekly_list, name='weekly_list'),
+    url(r'$', views.api_root),
 ])
--- django_base/django_base/urls.py
+++ django_base/django_base/urls.py
@@ -1,21 +1,20 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.views.decorators.cache import cache_page
 from . import views
 
 urlpatterns = [
     url(r'^here_is_normal_admin/', admin.site.urls),
-    url(r'^why/$', cache_page(60 * 10)(views.why)),
+    url(r'^why/$', views.why),
     url(r'channel/(?P<title>.+)/$',
-        cache_page(60 * 10)(views.channel), name='channel'),
+        views.channel, name='channel'),
     url(r'subcategory/(?P<sub>.+)/$',
-        cache_page(60 * 10)(views.sub_category), name='sub_category'),
+        views.sub_category, name='sub_category'),
     url(r'videos/(?P<title>.+)/$',
-        cache_page(60 * 10)(views.videos), name='videos'),
+        views.videos, name='videos'),
     url(r'category/(?P<title>.+)/$',
-        cache_page(60 * 10)(views.category), name='category'),
+        views.category, name='category'),
     url(r'^api/', include('api.urls')),
     url(r'^api-auth/', include(
         'rest_framework.urls', namespace='rest_framework')),
-    url(r'^', cache_page(60 * 10)(views.cate)),
+    url(r'^', views.cate),
 ]
