5e7d5f64ada8008d95f0036bd6675b96d154348f
Windson yang <wiwindson@outlook.com>
2019-04-13 13:16:29 +0800

    fix Nan

--- django_base/static/js/base.js
+++ django_base/static/js/base.js
@@ -235,6 +235,9 @@ function getCookie(name) {
 }
 
 function readSub(number) {
+    if (number == "NaN undefined") {
+        return " "
+    }
     var s = [" ", "K", "M"]
     var e = Math.floor(Math.log(number) / Math.log(1000));
     return (number / Math.pow(1000, e)).toFixed(1) + " " + s[e];
